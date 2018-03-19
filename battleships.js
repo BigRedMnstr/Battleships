0fbd0b7a92efd78685254661389147afbd74f3f8
BigRedMnstr <m-kucharski@wp.pl>
2018-03-19 09:41:15 +0100

    Added input handlers. Now the game is actually working

--- battleships.js
+++ battleships.js
@@ -126,3 +126,28 @@ function parseGuess(guess) {
 //controller.processGuess("B0");
 //controller.processGuess("B1");
 //controller.processGuess("B2");
+
+function init() {
+    var fireButton = document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton;
+    var guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
+}
+
+function handleFireButton(){
+    var guessInput = document.getElementById("guessInput");
+    var guess = guessInput.value;
+    controller.processGuess(guess);
+    
+    guessInput.value = "";
+}
+
+function handleKeyPress(e) {
+    var fireButton = document.getElementById("fireButton");
+    if (e.keyCode === 13) {
+        fireButton.click();
+        return false;
+    }
+}
+
+window.onload = init;
