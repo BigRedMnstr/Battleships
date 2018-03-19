e4d6c05ad41bd42b4219c277768240a250825a00
BigRedMnstr <m-kucharski@wp.pl>
2018-03-19 09:24:26 +0100

    Added and tested basic game logic

--- battleships.js
+++ battleships.js
@@ -1,24 +1,128 @@
 var view = {
-    displayMessage: function(msg){
+    displayMessage: function (msg) {
         var messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
     },
-    displayHit: function(location){
+    displayHit: function (location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "hit");
     },
-    displayMiss: function(location){
+    displayMiss: function (location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
     }
 };
 
-//view method test :
+////view methods test :
+//
+//view.displayMiss("00");
+//view.displayHit("34");
+//view.displayMiss("55");
+//view.displayHit("12");
+//view.displayMiss("25");
+//view.displayHit("26");
+//view.displayMessage("Is it really working?");
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Is it really working?");
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [
+        {locations: ["06", "16", "26"], hits: ["", "", ""]},
+        {locations: ["24", "34", "44"], hits: ["", "", ""]},
+        {locations: ["10", "11", "12"], hits: ["", "", ""]}
+    ],
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("STRUCK!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sinked the ship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed");
+        return false;
+    },
+    isSunk: function (ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+            return true;
+        }
+    }
+};
+
+////fire method test
+//model.fire("00");
+//model.fire("06");
+//model.fire("13");
+//model.fire("10");
+//model.fire("24");
+//model.fire("63");
+//model.fire("11");
+//model.fire("12");
+//model.fire("03");
+//model.fire("55");
+
+var controller = {
+    guesses: 0,
+    processGuess: function (guess) {
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sinked all ships in " + this.guesses + " tries! Good job Admiral!");
+            }
+        }
+    }
+};
+
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if (guess === null || guess.length !== 2) {
+        alert("You have to give me letter and number");
+    } else {
+        var firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+        if (isNaN(row) || isNaN(column)) {
+            alert("You have to give me coordinates!");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("The field is outside the board");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+
+////parseGuess method test 
+//console.log(parseGuess("A0"));
+//console.log(parseGuess("B4"));
+//console.log(parseGuess("F7"));
+//console.log(parseGuess("H2"));
+//console.log(parseGuess("D5"));
+
+////processGuess method test
+//controller.processGuess("A0");
+//controller.processGuess("A6");
+//controller.processGuess("B6");
+//controller.processGuess("C6");
+//controller.processGuess("C4");
+//controller.processGuess("D4");
+//controller.processGuess("E4");
+//controller.processGuess("B0");
+//controller.processGuess("B1");
+//controller.processGuess("B2");
