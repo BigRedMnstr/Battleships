0d7979b65d56b54624f504c64b9b879f7397c9ff
BigRedMnstr <m-kucharski@wp.pl>
2018-03-20 10:43:05 +0100

    Added randomly generated ships. Added collision detection to avoid adding 2 ships to 1 field. Application is up and running. Battleships ver 1.0

--- battleships.js
+++ battleships.js
@@ -29,15 +29,15 @@ var model = {
     shipLength: 3,
     shipsSunk: 0,
     ships: [
-        {locations: ["06", "16", "26"], hits: ["", "", ""]},
-        {locations: ["24", "34", "44"], hits: ["", "", ""]},
-        {locations: ["10", "11", "12"], hits: ["", "", ""]}
+        {locations: [0, 0, 0], hits: ["", "", ""]},
+        {locations: [0, 0, 0], hits: ["", "", ""]},
+        {locations: [0, 0, 0], hits: ["", "", ""]}
     ],
     fire: function (guess) {
         for (var i = 0; i < this.numShips; i++) {
             var ship = this.ships[i];
             var index = ship.locations.indexOf(guess);
-            
+
             if (ship.hits[index] === "hit") {
                 view.displayMessage("You already hit this field!");
                 return true;
@@ -45,7 +45,7 @@ var model = {
                 ship.hits[index] = "hit";
                 view.displayHit(guess);
                 view.displayMessage("STRUCK!");
-                
+
                 if (this.isSunk(ship)) {
                     view.displayMessage("You sinked the ship!");
                     this.shipsSunk++;
@@ -57,7 +57,7 @@ var model = {
         view.displayMessage("You missed");
         return false;
     },
-    
+
     isSunk: function (ship) {
         for (var i = 0; i < this.shipLength; i++) {
             if (ship.hits[i] !== "hit") {
@@ -65,6 +65,46 @@ var model = {
             }
         }
         return true;
+    },
+    generateShipLocations: function () {
+        var locations;
+        for (var i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip();
+            } while (this.collision(locations));
+            this.ships[i].locations = locations;
+        }
+    },
+    generateShip: function () {
+        var direction = Math.floor(Math.random() * 2);
+        var row, col;
+        if (direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+        var newShipLocations = [];
+        for (var i = 0; i < this.shipLength; i++) {
+            if (direction === 1) {
+                newShipLocations.push(row + "" + (col + i));
+            } else {
+                newShipLocations.push((row + i) + "" + col);
+            }
+        }
+        return newShipLocations;
+    },
+    collision: function (locations) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = model.ships[i];
+            for (var j = 0; j < locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
 
@@ -138,13 +178,15 @@ function init() {
     fireButton.onclick = handleFireButton;
     var guessInput = document.getElementById("guessInput");
     guessInput.onkeypress = handleKeyPress;
+    
+    model.generateShipLocations();
 }
 
-function handleFireButton(){
+function handleFireButton() {
     var guessInput = document.getElementById("guessInput");
     var guess = guessInput.value;
     controller.processGuess(guess);
-    
+
     guessInput.value = "";
 }
 
